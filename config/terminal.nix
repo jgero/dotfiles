4822b92c3996e59dabdc2e30a60577347f7bf0f1
Johannes Gerold <mail@jgero.me>
2022-08-11 22:01:46 +0200

    fix(terminal): increase font size

--- config/terminal.nix
+++ config/terminal.nix
@@ -1,12 +1,13 @@
 { config, pkgs, ... }: {
   programs.alacritty = {
     enable = true;
-	settings = {
-	  font = {
+    settings = {
+      font = {
         normal.family = "JetBrains Mono";
         bold.family = "JetBrains Mono";
         italic.family = "JetBrains Mono";
         bold_italic.family = "JetBrains Mono";
+        size = 14.0;
       };
     };
   };
