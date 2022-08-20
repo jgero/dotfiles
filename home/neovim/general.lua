013601f2b11994d0ec16e16abed5f9084541940c
Johannes Gerold <mail@jgero.me>
2022-08-20 11:58:45 +0200

    fix(embedded-shell): move from bash to zsh in neovim shell

--- home/neovim/general.lua
+++ home/neovim/general.lua
@@ -107,7 +107,7 @@ utils.map('n', '<CR>', '{ -> v:hlsearch ? ":nohl\\<CR>" : "\\<CR>" }()', { expr 
 -- exit terminal mode with escape
 utils.map('t', '<Esc>', '<C-\\><C-n>')
 -- open new terminal on the bottom
-utils.map('', '<leader>t', ':split term://bash<cr>')
+utils.map('', '<leader>t', ':split term://zsh<cr>')
 
 ---------------------------------------------------------------------------------------------------
 -- Telescope
