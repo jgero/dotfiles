b69f2060ea7f23def51702c8d2175c03c51d2895
jgero <git+commit@jgero.me>
2023-04-15 10:55:49 +0200

    feat(editor): use custom neovim nix flake as editor

--- home/zsh.nix
+++ home/zsh.nix
@@ -8,11 +8,12 @@
     };
     sessionVariables = {
       FZF_BASE = "{pkgs.fzf}";
-      GIT_EDITOR = "nvim";
+      GIT_EDITOR = "nix run ~/projects/init.lua --";
     };
     shellAliases = {
       addptask = "task add project:$(git rev-parse --show-toplevel | xargs basename)";
       ptask = "task project:$(git rev-parse --show-toplevel | xargs basename)";
+      nvim = "nix run ~/projects/init.lua --";
     };
     initExtra = ''
       ${builtins.readFile ../scripts/zsh/prompt.zsh}
