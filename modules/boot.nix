9c346524e7daf886665e6f1904dad80a49dea00a
Johannes Gerold <git+commit@jgero.me>
2023-05-08 15:04:16 +0200

    fix(disks): add ntfs modules to be able to write on unholy disks

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,4 +2,8 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.supportedFilesystems = [
+    "ntfs"
+  ];
 }
