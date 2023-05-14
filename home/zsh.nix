610ccd5edbcb5e993fe2c79bbc2cf94a6f63fe40
Johannes Gerold <git+commit@jgero.me>
2023-05-14 15:09:36 +0200

    feat(alias): add alias to import ssh key from FIDO device

--- home/zsh.nix
+++ home/zsh.nix
@@ -13,6 +13,7 @@
       ptask = "task project:$(git rev-parse --show-toplevel | xargs basename)";
       nvim = "nix run ~/projects/init.lua --";
       compress10 = "mogrify -quality 10";
+      importKey = "pushd ~/.ssh && ssh-keygen -K && mv id_ed25519_sk_rk id_ed25519_sk && mv id_ed25519_sk_rk.pub id_ed25519_sk.pub && popd";
     };
     initExtra = ''
       ${builtins.readFile ../scripts/zsh/prompt.zsh}
