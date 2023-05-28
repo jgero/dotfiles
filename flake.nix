5780ebcba1d9b921bb71133651b4156c7c380829
Johannes Gerold <git+commit@jgero.me>
2023-05-28 22:28:13 +0200

    chore(update): update to 23.11 release

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Who needs a working system anyways, me smash state";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     hyprland.url = "github:hyprwm/Hyprland";
