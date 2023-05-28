a51a32edccc636d5623fa250cc537a559b76e094
Johannes Gerold <git+commit@jgero.me>
2023-05-28 22:28:42 +0200

    chore(update): update to newest master

--- modules/impermanence.nix
+++ modules/impermanence.nix
@@ -1,7 +1,7 @@
 let
   impermanence = builtins.fetchTarball {
     url = "https://github.com/nix-community/impermanence/archive/master.tar.gz";
-    sha256 = "0nnp5g40lkkmfpvmc7sfws48fji3i0nz1k6pav8vkfk8pd1wl810";
+    sha256 = "095q3c1kyj7lpnn1i53c0158jh02avsm6xmkvql045xppkxfnk1b";
   };
 in
 {
