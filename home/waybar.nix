00403c280a73a53be85934fdbd993956d92f9d6a
Johannes Gerold <git+commit@jgero.me>
2023-05-30 20:02:40 +0200

    feat(bar): add more bar segments

--- home/waybar.nix
+++ home/waybar.nix
@@ -12,12 +12,36 @@ with lib;
       settings = {
         foo = {
           layer = "top";
-          modules-left = [ "wlr/workspaces" "hyprland/submap" ];
+          modules-left = [ "wlr/workspaces" "hyprland/submap" "hyprland/window" ];
           modules-center = [ "clock" ];
-          # add: bluetooth, sound, wifi, cpu, mem, temp, brightness
-          modules-right = [ "battery" ];
+          # add: nix store size?, pending flake update?, is system flake dirty?
+          modules-right = [ "cpu" "memory" "hyprland/language" "network" "bluetooth" "wireplumber" "battery" ];
 
           # modules
+          network = {
+            format = "NET";
+            format-wifi = "NET: {essid}";
+            format-ethernet = "NET: WIRED";
+          };
+          wireplumber = {
+            format = "VOL: {volume}%";
+          };
+          cpu = {
+            format = "CPU: {usage}%";
+          };
+          memory = {
+            format = "MEM: {percentage}%";
+            tooltip = false;
+          };
+          bluetooth = {
+            format-on = "BLUE: ON";
+            format-off = "BLUE: OFF";
+            format-connected = "BLUE: {num_connections}";
+          };
+          "hyprland/language" = {
+            format = "LANG: {}";
+            format-en = "EN/US";
+          };
           "wlr/workspaces" = {
             format = "{icon}";
           };
