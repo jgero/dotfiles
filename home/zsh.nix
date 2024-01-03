15211dd58c0cbc1cdf2fa7d86635666b8bc628b2
Johannes Gerold <git+commit@jgero.me>
2024-01-03 15:29:20 +0100

    fix: add new host key

--- home/zsh.nix
+++ home/zsh.nix
@@ -7,12 +7,10 @@
       enable = true;
       enableAutosuggestions = true;
       syntaxHighlighting.enable = true;
-      history.path = "/nix/persist/jgero/zsh_history";
       shellAliases = {
         ptask = "task project:$(git rev-parse --show-toplevel | xargs basename)";
         compress10 = "mogrify -quality 10";
         addK = "ssh-add -K";
-        hyprlandLogs = ''cat /tmp/hypr/$(ls -t /tmp/hypr/ | head -n 1)/hyprland.log'';
         sp = ''${select-project}/bin/select-project'';
         udm = "udisksctl mount -b";
       };
