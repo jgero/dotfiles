1ef65ed0d7e3798a119ebea2e394ddc1ccb12a35
Johannes Gerold <git+commit@jgero.me>
2024-01-25 19:32:07 +0100

    fix: add initial password

--- modules/user.nix
+++ modules/user.nix
@@ -4,8 +4,6 @@
     shell = pkgs.zsh;
     isNormalUser = true;
     extraGroups = [ "wheel" "libvirtd" ];
-    # generate hashed password with:
-    # $ nix-shell --run 'mkpasswd -m SHA-512 -s' -p mkpasswd
-    initialHashedPassword = "$6$DhUaJBDqSjzz5mGA$e2/Ru9Bm8ruIr7XnCgB0BvoKERGzivi/96.i8QzqIJtNCCygsDzY9yH8VqoQuXppTZLydjVCI55i.gGWFqEvd.";
+    initialPassword = "password";
   };
 }
