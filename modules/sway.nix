48cacdc3b74a7ba0f50bc752b42614d1772b325c
Johannes Gerold <git+commit@jgero.me>
2024-01-31 11:44:15 +0100

    feat(colors): add color setings

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,25 +1,36 @@
-{ pkgs, ... }:
-{
-  programs.sway.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
+{ pkgs, lib, ... }: {
+  options.jgero.colors = with lib; {
+    background = mkOption {
+      type = types.str;
+      description = "HEX value of background color in the format of '#000000'";
+    };
+    foreground = mkOption {
+      type = types.str;
+      description = "HEX value of foreground items and text color in the format of '#000000'";
+    };
   };
+  config = {
+    programs.sway.enable = true;
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+    };
 
-  security.polkit.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-  security.pam.services.swaylock = { };
+    security.polkit.enable = true;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+    security.pam.services.swaylock = { };
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+        };
       };
     };
   };
