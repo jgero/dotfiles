a5c7478766e5d75ae0eda159b45f59ee27dd480f
Johannes Gerold <git+commit@jgero.me>
2024-02-04 19:16:36 +0100

    chore(secrets): rekey secrets for nixpad

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   nixps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILULHj3/gAxtAorfFqsmWBNePbuKsI2ekBAZ0UMOA7Qv";
-  nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINm11FfQdzFVN5uKHIP7JJaN0FmLzMAsibaCo04135KC";
+  nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJFr/KSbiK5dPChL6yW6Q8Du2/o1DYARxm13vQ7Jn6F";
 in
 {
   "yubipin.age".publicKeys = [ nixps nixpad ];
