333a21e9f3b5a22b857e9e24a865ffd49ef8270f
Johannes Gerold <git+commit@jgero.me>
2024-02-10 17:18:56 +0100

    feat(secrets): add backup key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
   nixps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILULHj3/gAxtAorfFqsmWBNePbuKsI2ekBAZ0UMOA7Qv";
   nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJFr/KSbiK5dPChL6yW6Q8Du2/o1DYARxm13vQ7Jn6F";
+  backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1ioWmfKl0ljM4ZZtLV/xPLHmhIG5O3QljFAq+BDzal dotfiles backup key";
+  all = [ nixps nixpad backup ];
 in
 {
-  "yubipin.age".publicKeys = [ nixps nixpad ];
-  "restic-password.age".publicKeys = [ nixps nixpad ];
-  "backup-server-ssh-identity.age".publicKeys = [ nixps nixpad ];
+  "yubipin.age".publicKeys = all;
+  "restic-password.age".publicKeys = all;
+  "backup-server-ssh-identity.age".publicKeys = all;
 }
