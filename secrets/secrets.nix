03a76cdb05c6bea39d5489a9425149a5b024d925
Johannes Gerold <git+commit@jgero.me>
2024-02-13 19:48:06 +0100

    chore(secrets): rekey with new nixps key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  nixps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGEHimt+2Ao5Z4wj2SwoM7nudOQbEvYanW/f0FBiMcI root@nixps";
+  nixps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEt2OGDSgD6wMWQ2nDTnZRK3M7io8Vc93iiTcNNjnhM root@nixps";
   nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJFr/KSbiK5dPChL6yW6Q8Du2/o1DYARxm13vQ7Jn6F";
   backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1ioWmfKl0ljM4ZZtLV/xPLHmhIG5O3QljFAq+BDzal dotfiles backup key";
   all = [ nixps nixpad backup ];
