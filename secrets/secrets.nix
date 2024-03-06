a6936e6dd826d41fc8420e6f5c0ce737e5b1091e
Johannes Gerold <git+commit@jgero.me>
2024-03-06 10:45:38 +0100

    chore(secrets): add key for workpad

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   nixps = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEt2OGDSgD6wMWQ2nDTnZRK3M7io8Vc93iiTcNNjnhM root@nixps";
   nixpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGJFr/KSbiK5dPChL6yW6Q8Du2/o1DYARxm13vQ7Jn6F";
+  workpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdpTTiQyMvW3u6kK2rW53AiRd3aVQCXBrAMU4H016Jo";
   backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1ioWmfKl0ljM4ZZtLV/xPLHmhIG5O3QljFAq+BDzal dotfiles backup key";
-  all = [ nixps nixpad backup ];
+  all = [ nixps nixpad workpad backup ];
 in
 {
   "yubipin.age".publicKeys = all;
