23968dcc673d65292d57dbea36cac61475e9ba77
Johannes Gerold <git+commit@jgero.me>
2024-04-09 17:58:47 +0200

    fix(backups): replace custom systemd services with builtin options

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,8 +1,5 @@
-{ osConfig, ... }: {
+{
   programs.ssh = {
     enable = true;
-    matchBlocks."sftp.hidrive.strato.com" = {
-      identityFile = osConfig.age.secrets.backupIdentity.path;
-    };
   };
 }
--- modules/backup.nix
+++ modules/backup.nix
@@ -9,81 +9,17 @@ in
   };
   config = mkIf (cfg.enable)
     {
-      systemd.user.services =
-        let
-          remoteRepo = "sftp:jgero@sftp.hidrive.strato.com:/users/jgero/backups";
-          localRepo = "/run/media/jgero/backup-drive/restic";
-          passwordFile = config.age.secrets.resticPw.path;
-          backupPaths = "/data/jgero";
-          keep = {
-            daily = "7";
-            weekly = "4";
-            monthly = "6";
-            yearly = "3";
-          };
-        in
-        {
-          restic_backup = {
-            serviceConfig = {
-              Type = "oneshot";
-              ExecStart = "${pkgs.restic}/bin/restic backup -r ${remoteRepo} --password-file ${passwordFile} --one-file-system --tag systemd.timer ${backupPaths}";
-              ExecStartPost = "${pkgs.restic}/bin/restic forget -r ${remoteRepo} --password-file ${passwordFile} --tag systemd.timer --group-by \"paths,tags\" --keep-daily ${keep.daily} --keep-weekly ${keep.weekly} --keep-monthly ${keep.monthly} --keep-yearly ${keep.yearly}";
-            };
-            onFailure = [ "restic_unlock.service" ];
-            path = [
-              pkgs.openssh
-            ];
-          };
-          restic_backup_to_harddrive = {
-            after = [ "run-media-jgero-backup\\x2ddrive.mount" ];
-            requires = [ "run-media-jgero-backup\\x2ddrive.mount" ];
-            wantedBy = [ "run-media-jgero-backup\\x2ddrive.mount" ];
-            serviceConfig = {
-              ExecStart = "${pkgs.restic}/bin/restic backup -r ${localRepo} --password-file ${passwordFile} --one-file-system --tag systemd.timer ${backupPaths}";
-            };
-            unitConfig = {
-              OnSuccess = "restic_backup.service";
-            };
-          };
-          restic_prune = {
-            serviceConfig = {
-              Type = "oneshot";
-              ExecStart = "${pkgs.restic}/bin/restic -r ${remoteRepo} --password-file ${passwordFile} prune";
-            };
-            path = [
-              pkgs.openssh
-            ];
-          };
-          restic_unlock = {
-            serviceConfig = {
-              Type = "oneshot";
-              ExecStart = "${pkgs.restic}/bin/restic -r ${remoteRepo} --password-file ${passwordFile} unlock";
-            };
-            unitConfig = {
-              OnSuccess = "restic_backup.service";
-            };
-            path = [
-              pkgs.openssh
-            ];
-          };
-        };
-      systemd.user.timers = {
-        restic_backup = {
-          wantedBy = [ "timers.target" ];
-          partOf = [ "restic_backup.service" ];
-          timerConfig = {
-            OnCalendar = "daily";
-            Persistent = true;
-          };
-        };
-        restic_prune = {
-          wantedBy = [ "timers.target" ];
-          partOf = [ "restic_prune.service" ];
-          timerConfig = {
-            OnCalendar = "monthly";
-            Persistent = true;
-          };
-        };
+      services.restic.backups.jgero = {
+        repository = "s3:https://s3.hidrive.strato.com/restic-backups";
+        paths = [ "/home/jgero/data" "/home/jgero/media" ];
+        passwordFile = config.age.secrets.resticPw.path;
+        environmentFile = config.age.secrets.backupEnv.path;
+        pruneOpts = [
+          "--keep-daily 7"
+          "--keep-weekly 4"
+          "--keep-monthly 6"
+          "--keep-yearly 3"
+        ];
       };
     };
 }
