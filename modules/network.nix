7464e791f6adf65aa6bdb19af060f7d61ad7ef7e
Ruben Hönle <git@hoenle.xyz>
2024-05-30 21:10:56 +0200

    feat(network): add sGPN22 WiFi
    Co-authored-by: Johannes Gerold <commit+github@jgero.me>

--- modules/network.nix
+++ modules/network.nix
@@ -20,5 +20,41 @@ in
       networkmanager.enable = true;
       firewall.enable = true;
     };
+
+    networking.networkmanager.ensureProfiles.profiles = {
+      "GPN" = {
+        connection = {
+          id = "GPN";
+          type = "wifi";
+          autoconnect = true;
+          interface-name = "wlp2s0";
+        };
+        wifi = {
+          mode = "infrastructure";
+          ssid = "GPN";
+        };
+        wifi-security = {
+          key-mgmt = "wpa-eap";
+        };
+        "802-1x" = {
+          eap = "ttls;";
+          identity = "gpn";
+          password = "gpn";
+          phase2-auth = "pap";
+          domain-suffix-match = "radius.noc.gulas.ch";
+          ca-cert = "${builtins.fetchurl {
+            url = "https://letsencrypt.org/certs/isrgrootx1.pem";
+            sha256 = "sha256:1la36n2f31j9s03v847ig6ny9lr875q3g7smnq33dcsmf2i5gd92";
+            }}";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "stable-privacy";
+          method = "auto";
+        };
+      };
+    };
   };
 }
