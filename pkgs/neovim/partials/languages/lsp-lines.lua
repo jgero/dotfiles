e8e45efef3bac4f0d8b88f6959b063477ccd561e
Johannes Gerold <git+commit@jgero.me>
2025-11-10 08:08:46 +0100

    feat: enable lsp lines by default

--- pkgs/neovim/partials/languages/lsp-lines.lua
+++ pkgs/neovim/partials/languages/lsp-lines.lua
@@ -1,5 +1,5 @@
 require("lsp_lines").setup()
-vim.diagnostic.config({ virtual_text = false })
+vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
 -- toggle diagnostics to avoid clashes
 local diagnostics_active = true
 vim.keymap.set("n", "<leader>td", function()
