015879561fdd686b678eec6516c95645b3d90a44
Johannes Gerold <git+commit@jgero.me>
2025-12-06 16:05:00 +0100

    feat: update to newest NixOS stable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Who needs a working system anyways, me smash state";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
