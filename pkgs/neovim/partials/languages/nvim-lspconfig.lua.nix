066ca18a32fe3a464f5c483b3c648d5f6d2c8326
Johannes Gerold <git+commit@jgero.me>
2025-12-06 16:06:20 +0100

    fix: use new lsp config of nvim 0.11

--- pkgs/neovim/partials/languages/nvim-lspconfig.lua.nix
+++ pkgs/neovim/partials/languages/nvim-lspconfig.lua.nix
@@ -11,30 +11,39 @@
     end
     return cmd
   end
-  local on_attach = function(client, bufnr)
-    local nmap = function(keys, func, desc)
-      if desc then
-        desc = "LSP: " .. desc
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = "LSP: " .. desc
+        end
+        vim.keymap.set("n", keys, func, { buffer = args.buf, desc = desc })
       end
-      vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-    end
+      -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+      if client:supports_method('textDocument/completion') then
+        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+        -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+        -- client.server_capabilities.completionProvider.triggerCharacters = chars
 
-    if client.server_capabilities.documentFormattingProvider then
+        vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+      end
+      -- TODO: check if client supports method (client:supports_method) before creating keybindings
+      nmap("gd", vim.lsp.buf.definition, "[g]o to [d]efinition")
+      nmap("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
+      nmap("<leader>d", vim.lsp.buf.signature_help, "show signature help [d]ocumentation")
+      nmap("<leader>df", vim.diagnostic.open_float, "show [d]iagnostics [f]loat")
+      nmap("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ctions")
+      nmap("H", vim.lsp.buf.hover, "[H]over")
       nmap("<leader>lf", vim.lsp.buf.format, "[l]sp do [f]ormatting")
     end
-
-    nmap("gd", vim.lsp.buf.definition, "[g]o to [d]efinition")
-    nmap("<leader>rn", vim.lsp.buf.rename, "[r]e[n]ame")
-    nmap("<leader>d", vim.lsp.buf.signature_help, "show signature help [d]ocumentation")
-    nmap("<leader>df", vim.diagnostic.open_float, "show [d]iagnostics [f]loat")
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ctions")
-    nmap("H", vim.lsp.buf.hover, "[H]over")
-  end
-  local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-  local lspc = require("lspconfig")
-  lspc.lua_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  })
+  vim.lsp.config('*', {
+    capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    root_markers = { '.git' },
+  })
+  vim.lsp.config.lua_ls = {
     settings = {
       Lua = {
         workspace = {
@@ -46,45 +55,30 @@
         telemetry = { enable = false },
       },
     },
-    cmd = use_exec_or_fallback("lua-language-server", "${pkgs.sumneko-lua-language-server}/bin/lua-language-server"),
-  })
-  lspc.nil_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+    cmd = use_exec_or_fallback("lua-language-server", "${pkgs.lua-language-server}/bin/lua-language-server"),
+  }
+  vim.lsp.config.nil_ls = {
     cmd = use_exec_or_fallback("nil", "${pkgs.nil}/bin/nil"),
-  })
-  lspc.bashls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.bashls = {
     cmd = use_exec_or_fallback("bash-language-server", "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server", "start"),
-  })
-  lspc.clangd.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.clangd = {
     cmd = use_exec_or_fallback("clangd", "${pkgs.clang-tools}/bin/clangd"),
-  })
-  lspc.html.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.html = {
     cmd = use_exec_or_fallback("vscode-html-language-server", "${pkgs.vscode-langservers-extracted}/bin/vscode-html-language-server", "--stdio"),
-  })
-  lspc.cssls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.cssls = {
     cmd = use_exec_or_fallback("vscode-css-language-server", "${pkgs.vscode-langservers-extracted}/bin/vscode-css-language-server", "--stdio"),
-  })
-  lspc.jsonls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.jsonls = {
     cmd = use_exec_or_fallback("vscode-json-language-server", "${pkgs.vscode-langservers-extracted}/bin/vscode-json-language-server", "--stdio"),
-  })
-  lspc.ts_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.ts_ls = {
     cmd = use_exec_or_fallback("typescript-language-server", "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server", "--stdio"),
-
-  })
-  lspc.rust_analyzer.setup({
+  }
+  vim.lsp.config.rust_analyzer = {
     settings = {
       ['rust-analyzer'] = {
         cargo = {
@@ -92,25 +86,18 @@
         }
       }
     },
-    capabilities = capabilities,
-    on_attach = on_attach,
     cmd = use_exec_or_fallback("rust-analyzer", "${pkgs.rust-analyzer}/bin/rust-analyzer"),
-  })
-  lspc.gopls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.gopls = {
     cmd = use_exec_or_fallback("gopls", "${pkgs.gopls}/bin/gopls"),
-  })
-  lspc.templ.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
+  }
+  vim.lsp.config.templ = {
     cmd = use_exec_or_fallback("templ", "${pkgs.templ}/bin/templ", "lsp"),
-  })
-  lspc.golangci_lint_ls.setup({
-    capabilities = capabilities,
+  }
+  vim.lsp.config.golangci_lint_ls = {
     cmd = use_exec_or_fallback("golangci-lint-langserver", "${pkgs-unstable.golangci-lint-langserver}/bin/golangci-lint-langserver"),
     init_options = {
       command = { "${pkgs-unstable.golangci-lint}/bin/golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" },
     },
-  })
+  }
 ''
